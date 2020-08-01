4c728db1e30fb1dc9fcae97028a204dcf28208e9
thebrendanwhite <36090965+thebrendanwhite@users.noreply.github.com>
2020-07-31 20:53:27 -0400

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,8 @@
-import 
+import urllib.request
+import json
+
+url = 'https://api.datamuse.com/words?rel_trg=cow'
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+print(result)
+
